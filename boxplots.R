946b207fbacbe8352daf8915800fd9276635bb4c
dKvale <kvaled@gmail.com>
2014-04-30 13:38:50 -0500

    Update boxplots.R

--- boxplots.R
+++ boxplots.R
@@ -12,26 +12,37 @@ topcopc = na.omit(topcopc)
 
 #Switch to long format
 topcopc = melt(topcopc[,c(6:35)])
-topcopc$variable = as.character(topcopc$variable)
+names(topcopc)[1] = "Copc"
+topcopc$Copc = as.character(topcopc$Copc)
 
 #Separate Cancer and Non-cancer
 Canc_list=names(top[,c(7,23,35,9,13,15,33,31,27,25,11,19,21,29,17)])
 HI_list=names(top[,-c(1:5,7,23,35,9,13,15,33,31,27,25,11,19,21,29,17)])
-Canc_copc = subset(topcopc, topcopc$variable %in% Canc_list)
-HI_copc = subset(topcopc, topcopc$variable %in% HI_list)
+Canc_copc = subset(topcopc, topcopc$Copc %in% Canc_list)
+HI_copc = subset(topcopc, topcopc$Copc %in% HI_list)
 
 #Function to find outliers below 1% and above 99%
 Outliers.fun <- function(x) {  subset(x, x < quantile(x,.01) | x > quantile(x,.99)) }
 
-#Function to find quantiles for boxplot:  [ 1%,  25%, 50%, 75%, 99% ]
+#Function to find quantiles for boxplot, 1%,  25%, 50%, 75%, 99% 
 Quants.fun  <- function(x)
-   {
-    quants  <- quantile(x,  probs = c(0.01, 0.25, 0.5, 0.75, 0.99))
-    names(r) <- c("ymin", "lower", "middle", "upper", "ymax")
-    quants
-  }
+{
+  quants  <- quantile(x,  probs = c(0.01, 0.25, 0.5, 0.75, 0.99))
+  names(quants) <- c("ymin", "lower", "middle", "upper", "ymax")
+  quants
+}
 
-#Make boxplot with ggplot2, order pollutants by median, set vertical x-axis, set log scale
-ggplot(topcopc3, aes(x = reorder(variable, -value, median), y =value, fill=variable)) + 
-    stat_summary(fun.data=f, geom="boxplot") + theme_bw() + theme(axis.text.x = element_text(size =13, lineheight=1, angle = 45))+
-    stat_summary(fun.y = o, geom="point") + scale_y_log10()
+#Make boxplots with ggplot2, order pollutants by median, set vertical x-axis, set to log scale
+ggplot(HI_copc, aes(x = reorder(Copc, -value, median), y= value, fill=Copc)) +
+    stat_summary(fun.data=Quants.fun, geom="boxplot") + 
+    stat_summary(fun.y = Outliers.fun, geom="point") + 
+    scale_y_log10() + 
+    labs(x="COPC", y="Hazard index across all receptors", title = "Hazard Index Distributions in MnRisk") +
+    theme_bw() + theme(axis.text.x = element_text(size =13, angle = 45, hjust=1, vjust=1))
+
+ggplot(Canc_copc, aes(x = reorder(Copc, -value, median), y= value, fill= Copc)) +
+  stat_summary(fun.data=Quants.fun, geom="boxplot") + 
+  stat_summary(fun.y = Outliers.fun, geom="point") + 
+  scale_y_log10() + 
+  labs(x="COPC", y="Cancer risk across all receptors", title = "Cancer Risk Distributions in MnRisk") +
+  theme_bw() + theme(axis.text.x = element_text(size =13, angle = 45, hjust=1, vjust=1))

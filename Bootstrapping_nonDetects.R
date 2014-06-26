774f5699f0a5f2bdc6932716f5b2aaa13604842c
dKvale <kvaled@gmail.com>
2014-06-26 16:54:31 -0500

    Update Bootstrapping_nonDetects.R

--- Bootstrapping_nonDetects.R
+++ Bootstrapping_nonDetects.R
@@ -1,71 +1,79 @@
-## Bootstrapping the LCL and UCL for each group in a data frame with Non-detects or censored data ##
+###   Bootstrapping the LCL and UCL for each group in a data frame  ### 
+###   with Non-detects or censored data                             ###
 
-# bootStrap() function returns a summary table of bootstrap statistics for each group
-# percentile is used to find the % Upper Confidence Limit, 
-# 100-percentile is used for the % Lower Confidence Limit  
-bootStrap <- function(   data = dataTable, results = "result", censored = "censored",
-                       groups = "groupID", repeats = 2000, percentile = 95){
+# The bootStrap_NADA() function takes a censored column containing 0's for detects and 1's for non-detects,
+# or alternatively, TRUE for non-detected and FALSE for detected 
+bootStrap_NADA <- function(   data = dataTable, results = "result", censored = "censored",
+                       groups = "groupID", repeats = 2000, percentile = 0.95){
   library(NADA)
   library(dplyr)
-# Remove unnecessary columns to speed things up
+    
+# start stopwatch
+  start   <- proc.time()
+  count   <- 0
+  
+# Remove unnecessary columns to speed up repetitions
   data <- data[,c(results, groups, censored)]
-  data$groupID <- as.character(data$groupID)
+
+# Set censored column as logical (True/False)
+  data$censored<- as.logical(data$censored)
 
 # Rename columns
   names(data) <- c("result", "groupID", "censored")
   
+# Drop groups with less than 10% detects
+  drop_Cutoff <- 0.10
+  detect_data <- group_by(data, groupID) %.% summarize(detect_Fraction = length(subset(censored, censored==F))/length(censored) )
+  detect_data <- filter(detect_data, detect_Fraction >= drop_Cutoff)
+  data <- filter(data, groupID %in% detect_data$groupID)
+  
+# Remove factor from groupID
+  data$groupID <- as.character(data$groupID)
+
 # Get alpha in decimal form, default = 95% = 0.95
-  alpha <- (100-percentile)/100
-  
-# start stopwatch
-  start   <- proc.time()
-  count   <- 1
-  nGroups <- length(unique(data$groupID))
-  
+  alpha <- (1-percentile)
+
 # Suppress repeated warnings
   options(warn = -1)
   
 # Create empty tables to store results
   bootedMean        <- data.frame()
   bootstrap_Summary <- data.frame()
-  
-# Start bootstrapping each group one at a time to limit table size
-  for (group in unique(data$groupID)){
-    subGroup_table    <- filter(data, groupID == group)
-                 n    <- nrow(subGroup_table)
-# Print % done to screen
-    cat(round(count/nGroups*100),"%", sep="")
-    
-# Break into two groups to inrease speed
-    repeats1 <- repeats/2
+   
+# Break into two groups to inrease speed    
+    repeats1 <- round(repeats/2)
     repeats2 <- repeats - repeats1
     bootstrap_Results <- data.frame()
     bootstrap_Results2 <- data.frame()
-    
-    for (i in 1:repeats1){
-             bootedMean  <- data.frame(groupID=group, bootMeans=censNada(subGroup_table$result, subGroup_table$censored, sample(1:n, replace=T)))
-      bootstrap_Results  <- rbind(bootstrap_Results, bootedMean)
-} 
-    for (i in 1:repeats2){
-             bootedMean   <- data.frame(groupID=group, bootMeans=censNada(subGroup_table$result, subGroup_table$censored, sample(1:n, replace=T)))
-      bootstrap_Results2  <- rbind(bootstrap_Results2, bootedMean)
-}
-    bootstrap_Results <- rbind(bootstrap_Results,bootstrap_Results2)
-    bootstrap_Results <- summarize(bootstrap_Results, groupID = groupID[1], boot_LCL = quantile(bootMeans, probs= alpha, na.rm=T), boot_Mean= mean(bootMeans) , boot_UCL= quantile(bootMeans, probs=percentile, na.rm=T), b_StDev= sd(bootMeans))
-    bootstrap_Summary <- rbind(bootstrap_Summary, bootstrap_Results)
-    count <- count + 1
-    cat("\b\b\b\b")
 
+cat("Repetition: ")
+
+  for (i in 1:repeats1){
+    bootedMean <- group_by(data, groupID) %.% summarize(bootMeans = as.numeric(mean(cenfit(result[sample(1:length(result), replace=T)], censored))[1]))
+    bootstrap_Results  <- rbind_list(bootstrap_Results, bootedMean)
+    count <- count + 1
+    cat(count, ",", sep="")
+} 
+
+  for (i in 1:repeats2){
+    bootedMean <- group_by(data, groupID) %.% summarize(bootMeans = as.numeric(mean(cenfit(result[sample(1:length(result), replace=T)], censored))[1]))
+    bootstrap_Results2  <- rbind_list(bootstrap_Results2, bootedMean)
+    count <- count + 1
+    cat(count, ",", sep="")
 }
+
+  bootstrap_Results <- rbind_list(bootstrap_Results,bootstrap_Results2)
+  bootstrap_Summary<- group_by(bootstrap_Results, groupID) %.% summarize(boot_LCL = quantile(bootMeans, probs= alpha, na.rm=T), boot_Mean= mean(bootMeans) , boot_UCL= quantile(bootMeans, probs=percentile, na.rm=T), b_StDev= sd(bootMeans))
+
   # Bring back original groups column name
-  names(bootstrap_Summary)[1] <- c(groups)
+  names(bootstrap_Summary)[1]  <- c(groups)
   row.names(bootstrap_Summary) <- NULL 
   
   # Turn warnings back on
   options(warn=1)
   
   # Show time elapsed
-  proc.time() - start
-  head(bootstrap_Summary)
-     
+  print(proc.time() - start)
+  print(head(bootstrap_Summary))
+  return(bootstrap_Summary)   
 }

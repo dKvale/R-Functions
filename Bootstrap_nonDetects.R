d81d42f88dabb69ea3e2193736a6c5ff212073c0
dKvale <kvaled@gmail.com>
2014-06-27 17:05:34 -0500

    Update and rename Bootstrapping_nonDetects.R to Bootstrap_nonDetects.R

---
+++ Bootstrap_nonDetects.R
@@ -0,0 +1,129 @@
+###-----------------------------------------------------------------------------------###
+#
+#     File:     Bootstrap_nonDetects.R
+#  
+#     Purpose:  Using the NADA and dplyr package, the function below Bootstraps 
+#               the 5% LCL and 95% UCL of the Kaplan-Meier mean for each group or
+#               pollutant in a data frame containing non-detected or censored data         
+#                                          
+###-----------------------------------------------------------------------------------###
+
+library(NADA)
+library(dplyr)
+
+#************ Example  ************# 
+
+# Bootstrap the 95% UCL and 5% LCL of ozone measurements for each month in "airquality" dataset
+ozone <- airquality
+ozone$Non_detect <- is.na(ozone$Ozone)
+ozone[is.na(ozone$Ozone),"Ozone"]<-0
+head(ozone)
+#  Ozone Solar.R Wind Temp Month Day Non_detect
+#1    41     190  7.4   67     5   1      FALSE
+#2    36     118  8.0   72     5   2      FALSE
+#3    12     149 12.6   74     5   3      FALSE
+#4    18     313 11.5   62     5   4      FALSE
+#5     0      NA 14.3   56     5   5       TRUE
+#6    28      NA 14.9   66     5   6      FALSE
+
+group_by(ozone, Month) %>% summarize(Ozone_KapM_mean = mean(cenfit(Ozone, Non_detect))[1])
+#  Month   Ozone_KapM_mean
+#1     5              20.0
+#2     6              17.2
+#3     7              50.7
+#4     8              51.7
+#5     9              30.6
+
+ozone_Booted <- boot_NADA(data=ozone, results="Ozone", censored="Non_detect", groups = "Month", repeats=100)
+head(ozone_Booted)
+#  Month  boot_LCL boot_Mean boot_UCL boot_StDev
+#1     5     13.77        44      126         45
+#2     6      0.72        15       53         21
+#3     7     36.82        56       96         19
+#4     8     36.27        63      120         30
+#5     9     23.44        34       55         11
+
+#*********** End Example *****************# 
+
+# The boot_NADA() function takes a censored column containing 0's for detects and 1's for
+# non-detects, or alternatively FALSE for detects and TRUE for non-detects
+boot_NADA <- function(data = dataTable, results = "result", censored = "censored", groups = "groupID", repeats = 2000, percentile = 0.95){
+  
+  library(NADA)
+  library(dplyr)
+  
+  # Remove unnecessary columns to speed up repetitions
+  data <- data[,c(results, groups, censored)]
+  
+  # Rename columns
+  names(data) <- c("result", "groupID", "censored")
+  
+  # Set censored column as logical (True/False)
+  data$censored<- as.logical(data$censored)
+
+  # Drop groups with less than 10% detects
+  drop_Cutoff <- 0.10
+  detect_data <- group_by(data, groupID) %.% summarize(detect_Fraction = length(subset(censored, censored==F))/length(censored) )
+  detect_data <- filter(detect_data, detect_Fraction >= drop_Cutoff)
+  data <- filter(data, groupID %in% detect_data$groupID)
+  
+  # Set NA's and negative result values at 0.0
+  data[is.na(data$result), "result"] <- 0
+  data[data$result < 0, "result"] <- 0
+  
+  # Remove factor from groupID
+  data$groupID <- as.character(data$groupID)
+  
+  # Get alpha in decimal form, default = 95% = 0.95
+  alpha <- (1-percentile)
+  
+  # Start stopwatch
+  start   <- proc.time()
+  count   <- 0
+  
+  # Suppress repeated warnings
+  options(warn = -1)
+  
+  # Initiate final summary table
+  bootstrap_Summary <- data.frame()
+  
+  # Start bootstrapping one group at a time to limit table size
+  for (group in unique(data$groupID)){
+    
+    subGroup_table <- filter(data, groupID == group)
+    data$groupID   <- as.character(data$groupID)
+    n <- nrow(subGroup_table)
+    
+    # Print % done to screen
+    nGroups <- length(unique(data$groupID))
+    cat(ceiling(count/nGroups*100),"% ", sep="")
+    
+    # Initiate final summary table
+    bootstrap_Results <- data.frame()
+    
+    # Use Lapply to repeat Cenfit from NADA package and bootstrap the mean of the censured data
+    bootedMeans  <- lapply(1:repeats, FUN= function(x) mean(cenfit(subGroup_table$result[sample(1:n, replace=T)], subGroup_table$censored))[1])
+  
+    # Summarize the booted Cenfit means: LCL, UCL, Mean, and Std. Dev
+    bootstrap_Results <- data.frame(groupID = group, bootMeans=unlist(bootedMeans))
+    bootstrap_Results <- summarize(bootstrap_Results, groupID = groupID[1], boot_LCL = quantile(bootMeans, probs= alpha, na.rm=T), boot_Mean= mean(bootMeans) , boot_UCL= quantile(bootMeans, probs=percentile, na.rm=T), boot_StDev= sd(bootMeans))
+    bootstrap_Summary <- rbind(bootstrap_Summary, bootstrap_Results)
+    
+    count <- count + 1
+    cat("\b\b\b\b\b")
+  }
+  
+  # Bring back the original group name
+  names(bootstrap_Summary)[1]  <- groups
+  row.names(bootstrap_Summary) <- NULL
+  
+  # Turn warnings back on
+  options(warn=1)
+  
+  # Show time elapsed
+  print(proc.time() - start)
+  cat("\n")
+  
+  print(head(bootstrap_Summary))
+  return(bootstrap_Summary)    
+}

3a16b7c8358b2546cec6f4623751b645c0b187bb
dKvale <kvaled@gmail.com>
2014-06-30 17:32:12 -0500

    Update and rename boot_NADA.R to bootNADA.R

--- bootNADA.R
+++ bootNADA.R
@@ -1,24 +1,24 @@
 ###-----------------------------------------------------------------------------------###
 #
-#     File:     bootNADA.R
-#  
-#     Purpose:  Using the NADA and dplyr package, the function below Bootstraps 
-#               the 5% LCL and 95% UCL of the Kaplan-Meier mean for each group or
-#               pollutant in a data frame that contains non-detected or censored data         
-#                                          
+# File: bootNADA.R
+#
+# Purpose: Using the NADA and dplyr package, the function below Bootstraps
+# the 5% LCL and 95% UCL of the Kaplan-Meier mean for each group or
+# pollutant in a data frame that contains non-detected or censored data
+#
 ###-----------------------------------------------------------------------------------###
 
 library(NADA)
 library(dplyr)
 
-#************ Example  ************# 
+#************ Example ************#
 
 # Bootstrap the 95% UCL and 5% LCL of ozone measurements for each month in "airquality" dataset
 ozone <- airquality
 ozone$Non_detect <- is.na(ozone$Ozone)
 ozone[is.na(ozone$Ozone),"Ozone"]<-0
-head(ozone)
-#  Ozone Solar.R Wind Temp Month Day Non_detect
+
+##  Ozone Solar.R Wind Temp Month Day Non_detect
 #1    41     190  7.4   67     5   1      FALSE
 #2    36     118  8.0   72     5   2      FALSE
 #3    12     149 12.6   74     5   3      FALSE
@@ -27,23 +27,23 @@ head(ozone)
 #6    28      NA 14.9   66     5   6      FALSE
 
 group_by(ozone, Month) %>% summarize(Ozone_KapM_mean = mean(cenfit(Ozone, Non_detect))[1])
-#  Month   Ozone_KapM_mean
-#1     5              20.0
-#2     6              17.2
-#3     7              50.7
-#4     8              51.7
-#5     9              30.6
+##  Month Ozone_KapM_mean
+#1    5      20.0
+#2    6      17.2
+#3    7      50.7
+#4    8      51.7
+#5    9      30.6
 
-ozone_Booted <- bootNADA(data=ozone, results="Ozone", censored="Non_detect", groups = "Month", repeats=100)
-head(ozone_Booted)
-#  Month  boot_LCL boot_Mean boot_UCL boot_StDev
-#1     5     13.77        44      126         45
-#2     6      0.72        15       53         21
-#3     7     36.82        56       96         19
-#4     8     36.27        63      120         30
-#5     9     23.44        34       55         11
+ozone.Booted <- bootNADA(data=ozone, results="Ozone", censored="Non_detect", groups = "Month", repeats=100)
+head(ozone.Booted)
+##   Month boot_LCL boot_Mean boot_UCL boot_StDev
+#1     5       14        20       29        4.3
+#2     6       14        18       23        2.8
+#3     7       41        50       60        5.8
+#4     8       39        52       67        8.6
+#5     9       24        31       38        4.3
 
-#*********** End Example *****************# 
+#*********** End Example *****************#
 
 # The boot_NADA() function takes a censored column containing 0's for detects and 1's for
 # non-detects, or alternatively FALSE for detects and TRUE for non-detects
@@ -60,7 +60,7 @@ bootNADA <- function(data = dataTable, results = "result", censored = "censored"
   
   # Set censored column as logical (True/False)
   data$censored<- as.logical(data$censored)
-
+  
   # Drop groups with less than 10% detects
   drop_Cutoff <- 0.10
   detect_data <- group_by(data, groupID) %.% summarize(detect_Fraction = length(subset(censored, censored==F))/length(censored) )
@@ -78,8 +78,8 @@ bootNADA <- function(data = dataTable, results = "result", censored = "censored"
   alpha <- (1-percentile)
   
   # Start stopwatch
-  start   <- proc.time()
-  count   <- 0
+  start <- proc.time()
+  count <- 0
   
   # Suppress repeated warnings
   options(warn = -1)
@@ -91,7 +91,7 @@ bootNADA <- function(data = dataTable, results = "result", censored = "censored"
   for (group in unique(data$groupID)){
     
     subGroup_table <- filter(data, groupID == group)
-    data$groupID   <- as.character(data$groupID)
+    data$groupID <- as.character(data$groupID)
     n <- nrow(subGroup_table)
     
     # Print % done to screen
@@ -101,9 +101,15 @@ bootNADA <- function(data = dataTable, results = "result", censored = "censored"
     # Initiate final summary table
     bootstrap_Results <- data.frame()
     
+    # Define function to record cenfit mean of re-sampled table
+    getCenMean <- function(x=6){
+          random.rows <- sample(1:n, replace=T)
+          mean(cenfit(subGroup_table$result[random.rows], subGroup_table$censored[random.rows]))[1]
+}
+
     # Use Lapply to repeat Cenfit from NADA package and bootstrap the mean of the censured data
-    bootedMeans  <- lapply(1:repeats, FUN= function(x) mean(cenfit(subGroup_table$result[sample(1:n, replace=T)], subGroup_table$censored))[1])
-  
+    bootedMeans <- lapply(1:repeats, FUN = getCenMean)
+    
     # Summarize the booted Cenfit means: LCL, UCL, Mean, and Std. Dev
     bootstrap_Results <- data.frame(groupID = group, bootMeans=unlist(bootedMeans))
     bootstrap_Results <- summarize(bootstrap_Results, groupID = groupID[1], boot_LCL = quantile(bootMeans, probs= alpha, na.rm=T), boot_Mean= mean(bootMeans) , boot_UCL= quantile(bootMeans, probs=percentile, na.rm=T), boot_StDev= sd(bootMeans))
@@ -114,7 +120,7 @@ bootNADA <- function(data = dataTable, results = "result", censored = "censored"
   }
   
   # Bring back the original group name
-  names(bootstrap_Summary)[1]  <- groups
+  names(bootstrap_Summary)[1] <- groups
   row.names(bootstrap_Summary) <- NULL
   
   # Turn warnings back on
@@ -125,5 +131,5 @@ bootNADA <- function(data = dataTable, results = "result", censored = "censored"
   cat("\n")
   
   print(head(bootstrap_Summary))
-  return(bootstrap_Summary)    
+  return(bootstrap_Summary)
 }

66efed7e2262f960c40474c00d943e0040ac53bb
Dorian <dKvale@users.noreply.github.com>
2014-08-08 12:43:28 -0500

    Create plot1.R

---
+++ Coursera/Exploratory_Data/Project_1/plot1.R
@@ -0,0 +1,7 @@
+# plot1.R creates a frequency historgram of the global active power data and saves it as plot1.png
+
+# The data is online at the UC Irvine Machine Learning Repository,
+# and can be downloaded from https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip
+
+# Load data
+
